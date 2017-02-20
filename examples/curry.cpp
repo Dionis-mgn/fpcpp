50a41a6c1a317c6ada6af29cb864b94dc00a35e3
Denis Glebov <dionis.mgn@gmail.com>
2017-02-21 00:19:43 +0500

    'curry' tests

--- examples/curry.cpp
+++ examples/curry.cpp
@@ -14,7 +14,7 @@ int main()
 
 	auto mulL = [](int i, int j, int k, int m) { return i * j * k * m; };
 
-	auto curried = curry(mulL, 3, 4, _1, _2);
+	auto curried = curry(mulL, 3, _1, 4, _2);
 	std::cout
 		<< curried(5, 2) << std::endl
 		<< curried(1, _3)(false, "IBM", 2) << std::endl // false and "IBM" arguments will be ignored
--- tests/curry.cpp
+++ tests/curry.cpp
@@ -10,3 +10,23 @@ TEST(curry, pass_all_arguments)
 	auto curried = curry(mulL, 1, 2, 3, 4);
 	EXPECT_EQ(curried(), 24);
 }
+
+TEST(curry, pass_no_arguments)
+{
+	auto mulL = [](int i, int j, int k, int m) { return i * j * k * m; };
+
+	using namespace std::placeholders;
+
+	auto curried = curry(mulL, _1, _2, _3, _4);
+	EXPECT_EQ(curried(1, 2, 3, 4), 24);
+}
+
+TEST(curry, ignore_extra_leading_args)
+{
+	auto id = [](int i) { return i; };
+
+	using namespace std::placeholders;
+
+	auto curried = curry(id, _5);
+	EXPECT_EQ(curried(1, 2, 3, 4, 5), 5);
+}
