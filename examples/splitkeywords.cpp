9dfbc50467d83fb5765e9199192a4c1ea104a4a2
Denis Glebov <dionis.mgn@gmail.com>
2017-03-16 01:48:03 +0500

    Split Keywords example improvements

--- examples/splitkeywords.cpp
+++ examples/splitkeywords.cpp
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * FPCPP Example — Split Keywords
+ *
+ *  This example shows two ways to parse comma-separated text
+ * as a set of keywords/tags using FPCPP. "a,b,c" -> "a","b","c".
+ ******************************************************************************/
+
 #include <iostream>
 #include <vector>
 
@@ -8,7 +15,9 @@ using keywordList = std::vector<std::string>;
 
 int main()
 {
-	auto debugPrint = [](const auto &v) {
+	/* DEBUG PRINT
+	 * The only purpose of this function - print some collectioon to STDOUT. */
+	auto printContainer = [](const auto &v) {
 		std::cout << "---CONTAINER---" << std::endl;
 		for (auto &i : v)
 			std::cout << i << std::endl;
@@ -17,6 +26,25 @@ int main()
 		return v;
 	};
 
+	/* TWO PASSES VERSION
+	 * It splits string to collection of substrings and iterate it twice.
+	 * First time it trims each of the substring and second time it removes all
+	 * empty substrings.
+	 * So, performance is not as good as it may be. Still, this implementation is
+	 * pretty easy.*/
+	auto splitKeywords_twoPasses =
+	  pipe(split(','), map(trim), reject(is_empty()), printContainer);
+
+	/* This variant is also correct (there are no printContainer in pipe call):
+	auto splitKeywords = pipe(split(','), map(trim), reject(is_empty()));
+	printContainer(splitKeywords("some,data, string,")); */
+
+	/* ONE PASS VERSION
+	 * This version splits string to collection of substrings and uses reduce to
+	 * trim and reject empty substrings in one pass.
+	 * This version looks much uglier (in the main because I have to implement
+	 * reducer — valueFormatter function) but at the same time it is much faster.
+	 */
 	auto valueFormatter = [](const std::string &value, keywordList &accumulator) {
 		std::string trimmed = trim(value);
 		if (!trimmed.empty())
@@ -25,12 +53,18 @@ int main()
 
 	keywordList acc;
 
-	auto splitKeywords =
-	  pipe(split(','), reduce(valueFormatter, acc), debugPrint);
+	auto splitKeywords_onePass =
+	  pipe(split(','), reduce(valueFormatter, acc), printContainer);
 
-	const std::string testStr = "  all ,this,, , keywords, should,   ,,be printed"
+	/* TEST DATA */
+	const std::string testStr = "  all ,this,, , keywords, will,   ,,be printed"
 	                            ", separately   , without trailing ws,,,Yep!    ";
-	splitKeywords(testStr);
+
+	/* CALLS TO FUNCTIONS */
+	std::cout << "Two Passes version:\n" << std::endl;
+	splitKeywords_twoPasses(testStr);
+	std::cout << "\nOne Pass version:\n" << std::endl;
+	splitKeywords_onePass(testStr);
 
 	return 0;
 }
